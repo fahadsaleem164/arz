00064f7bc92ade34b099dab619c123965beeb35d
fahadsaleem164 <igz.iwd.25@gmail.com>
2021-05-29 03:23:01 -0700

    'add'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,175 @@
-import React from "react"
-import { PageWrapper } from "~components/Core"
-import HeroSection from "~sections/marketing/Hero"
-import ServiceSection from "~sections/marketing/Service"
-import FeatureSection from "~sections/marketing/Features"
-import ContentSectionOne from "~sections/marketing/ContentOne"
-import ContentSectionTwo from "~sections/marketing/ContentTwo"
-import TestimonialSection from "~sections/marketing/Testimonial"
-import CounterSection from "~sections/marketing/Counter"
-import CtaSection from "~sections/marketing/Cta"
-import FooterOne from "~sections/marketing/FooterOne"
-import HeaderButton from "~sections/marketing/Header"
+import React from "react";
+import { PageWrapper } from "~components/Core";
+import HeroSection from '~sections/digital/Hero'
+import ServicesSection from '~sections/digital/Services'
+import AboutSection from '~sections/digital/About'
+import ContentSectionOne from '~sections/digital/ContentOne'
+import ContentSectionTwo from '~sections/digital/ContentTwo'
+// import TeamSection from '~sections/digital/Team'
+import PortfolioSection from '~sections/digital/Portfolio'
+import PromoSection from '~sections/digital/Promo'
+import HeaderButton from '~sections/digital/Header'
+import FooterSix from '~sections/digital/FooterSix'
+import { StaticQuery, graphql } from "gatsby"
+
 const header = {
-  headerClasses: "site-header site-header--menu-start light-header site-header--sticky",
-  containerFluid:true,
-  // customLogo: Images.HeaderLogo,
-  buttonBlock: (
-    <HeaderButton
-      className="ms-auto d-none d-xs-inline-flex"
-      btnOneText="Login"
-      btnTwoText="Get started"
-      mr="15px"
-      mrLG="0"
-    />
-  ),
+  headerClasses: "site-header site-header--menu-end dark-header site-header--sticky",
+  containerFluid:false,
+  buttonBlock : <HeaderButton className="d-none d-sm-flex" children="Free SEO Analysis"/>,
+  darkLogo:false,
 }
 
-export default function Marketing() {
+const digital = () => (
+
+
+<StaticQuery query={digitalPageData} render={data=>{
+
+  console.log(data.mainSection)
+   
   return (
     <PageWrapper headerConfig={header}>
-      <HeroSection/>
-      <ServiceSection/>
-      <FeatureSection/>
-      <ContentSectionOne/>
-      <ContentSectionTwo/>
-      <TestimonialSection />
-      <CounterSection/>
-      <CtaSection />
-      <FooterOne/>
+       <HeroSection mainSection={data.mainSection} />
+       <ServicesSection whatWeDo={data.whatWeDo}/>
+       <AboutSection whyChooseUs={data.whyChooseUs} />
+        <ContentSectionOne whoWeAre={data.whoWeAre}/>
+        <ContentSectionTwo ourMission={data.ourMission}/>
+        {/* <TeamSection/> */}
+       <PortfolioSection caseStudy={data.caseStudy}/>
+       <PromoSection/>
+       <FooterSix/>
     </PageWrapper>
   )
-}
+}}/>
+)
+
+export const digitalPageData = graphql`
+  {
+    whoWeAre :  allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/whoWeAre/"}} 
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            description
+            image1
+            image2
+          }
+          
+          excerpt
+        }
+      }
+    }
+
+    ourMission: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/ourMission/"}}
+    )  {
+      edges {
+        node {
+          id
+          frontmatter {
+            
+            title
+            description
+            image1
+            image2
+            link
+            
+          }
+        }
+      }
+    }
+
+    whyChooseUs: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/whyUseUs/"}}
+    )  {
+      edges {
+        node {
+          id
+          frontmatter {
+            
+            title
+            description
+            video  
+            projects_done
+            conservation_rate
+            return_happy_customer
+          }
+        }
+      }
+    }
+
+
+    caseStudy: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/caseStudy/"}}
+    )  {
+      edges {
+        node {
+          frontmatter {
+            title
+            link
+            image2
+            image1
+            description
+            Case_Studies {
+              title
+              link
+              image
+              description
+            }
+          }
+        }
+      }
+    }
+
+
+    whatWeDo: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/ourServices/"}}
+    )  {
+      edges {
+        node {
+          frontmatter {
+            title
+            link
+            description
+            ourservices {
+              title
+              description
+              link
+              image
+            }
+           
+          }
+        }
+      }
+    }
+
+
+    mainSection: allMarkdownRemark(filter: {fileAbsolutePath: {regex: "posts/mainSection/"}}
+    )  {
+      edges {
+        node {
+          frontmatter {
+            title
+            link
+            text
+          }
+        }
+      }
+    }
+
+
+
+    
+
+
+    
+
+
+   
+
+
+
+
+    
+
+
+  }`
+
+export default digital;
